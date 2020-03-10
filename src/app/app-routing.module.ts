9a2ed3b4bce52efaf1530cfc1e7d566c39e0580b
NayeemAhmedTennant <50210308+NayeemAhmedTennant@users.noreply.github.com>
2020-03-10 14:08:51 -0500

    Simple Angular Test App
    Simple Angular Test App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
 
-const routes: Routes = [];
+const routes: Routes = [  { path: 'auth-callback', component: AuthCallbackComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthCallbackComponent
   ],
   imports: [
     BrowserModule,
